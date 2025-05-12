69f98cca5e28e46b232ea3cc9ac78d0ccb1eb42e
so-ya <soya.hironaka@gmail.com>
2025-05-12 12:45:17 +0900

    Initial commit: Laravel API and React TODO app

---
+++ laravel-api-v2/routes/api.php
@@ -0,0 +1,51 @@
+<?php
+
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Models\Task;
+
+Route::get('/tasks', function () {
+    return Task::all(); // DBから全件取得
+});
+
+Route::post('/tasks', function (Request $request) {
+    $validated = $request->validate([
+        'title' => 'required|string|max:225'
+    ]);
+
+    $task = Task::create([
+        'title' => $validated['title'],
+        'completed' => false,
+    ]);
+
+    return response()->json($task);
+
+});
+
+
+Route::put('/tasks/{id}', function ($id, Request $request) {
+    $validated = $request->validate([
+        'title' => 'required|string|max:255',
+        'completed' => 'boolean',
+    ]);
+    
+
+    $task = Task::findOrFail($id);
+    $task->update($validated);
+
+    return response()->json($task);
+
+});
+
+Route::delete('/tasks/{id}', function($id) {
+    $task = Task::findOrFail($id);
+    $task->delete;
+
+    return response()->json(['message' => '削除しました']);
+});
+
+
+
+
+
---
+++ todo-app/tailwind.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",  // ← ts, tsx も一応追加しておこう。保険。
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
